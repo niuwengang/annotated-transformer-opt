470e6126b84832ca27026026d798eafff108b9c6
牛文港 <niuwengang1@xiaomi.com>
2026-07-02 19:36:46 +0800

    fix:注释补充

--- transformer_model.py
+++ transformer_model.py
@@ -287,8 +287,8 @@ def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0
     model = EncoderDecoder(
         Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
         Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
-        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
-        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),#源嵌入
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),#目标嵌入
         Generator(d_model, tgt_vocab),
     )
 
